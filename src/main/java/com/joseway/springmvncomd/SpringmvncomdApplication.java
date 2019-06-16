b6b17de4467f498e52b62aafd1e56c9989a770b4
noella <noellanzeba@gmail.com>
2019-06-16 17:07:26 +0100

    prject creation

---
+++ src/main/java/com/joseway/springmvncomd/SpringmvncomdApplication.java
@@ -0,0 +1,13 @@
+package com.joseway.springmvncomd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvncomdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvncomdApplication.class, args);
+	}
+
+}
